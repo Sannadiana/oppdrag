dc4c13220b806eb634ce14d1d52363b52c0b0277
sannadianae <sanna-diana@hotmail.comm>
2019-02-15 17:57:56 +0100

    oppdrag 1 fullført

--- oppdrag1/scripts/bordInter.js
+++ oppdrag1/scripts/bordInter.js
@@ -1,13 +1,5 @@
 $(function() {
     $(".card")
-        /*Hvis man kan i en løsning stilsette fra css gjør det. 
-        Ellers så kan JQuery style når det er dynamisk, bruker preferanser.*/
-        .css({
-            "background-color": "#A0A0A0",
-            "width": "40%",
-            "padding": "1rem",
-            "border": "1px solid rgb(100, 100, 0)"
-        })
         .draggable({
             start: function() {
                 $(this).toggleClass("dragged");
@@ -15,45 +7,34 @@ $(function() {
             stop: function() {
                 $(this).toggleClass("dragged");
             },
-            drag: function() {
-
-            }
         });
 
-    $("#in-progress")
-        /*Hvis man kan i en løsning stilsette fra css gjør det. 
-        Ellers så kan JQuery style når det er dynamisk, bruker preferanser.*/
+    $("#to-do")
 
     .droppable({
-        over: function() {
+        drop: function(event, ui) {
+            ui.draggable.css("background-color", "#0080ff");
+        }
 
-        },
-        out: function() {
+    });
 
-        },
+    $("#in-progress")
+
+    .droppable({
         drop: function(event, ui) {
             ui.draggable.css("background-color", "orange")
-
         }
 
     });
 
     $("#done")
-        /*Hvis man kan i en løsning stilsette fra css gjør det. 
-        Ellers så kan JQuery style når det er dynamisk, bruker preferanser.*/
+        .droppable({
 
-    .droppable({
-        over: function() {
+            drop: function(event, ui) {
+                ui.draggable.css("background-color", "green")
 
-        },
-        out: function() {
+            }
 
-        },
-        drop: function(event, ui) {
-            ui.draggable.css("background-color", "green")
-
-        }
-
-    });
+        });
 
 })
--- oppdrag1/scripts/content.js
+++ oppdrag1/scripts/content.js
@@ -2,26 +2,39 @@ let cardApp = new Vue({
     el: "#flow-app",
     data: {
         cardList: [{
-            name: "Design registration page:",
-            task: "-Use fonts xyz and colorpalet æøå",
-            responsable: "Knut Iversen",
-            image: "img/knutIversen.png"
+                name: "Design registration page",
+                task: "* Use fonts xyz and colorpalet æøå",
+                responsable: "Knut Iversen",
+                image: "images/knutIversen.png"
 
-        }, {
-            name: "Design logo:",
-            task: "-For web, tablet and mobile",
-            responsable: "Anne Aur",
-            image: "img/anneAur.png"
-        }, {
-            name: "Design login page:",
-            task: "-Login by mail, phone nr. or SoMe",
-            responsable: "Knut Are",
-            image: "img/knutAre.png"
-        }, {
-            name: "Design content page:",
-            task: "-News, updates, menu",
-            responsable: "Lise Charlotte",
-            image: "img/liseChar.png"
-        }]
+            }, {
+                name: "Design logo",
+                task: "* Make it for web, tablet and mobile",
+                responsable: "Anne Aur",
+                image: "images/anneAur.png"
+            }, {
+                name: "Design login page",
+                task: "* Design for login by mail, phone nr. or SoMe",
+                responsable: "Knut Are",
+                image: "images/knutAre.png"
+            }, {
+                name: "Design content page",
+                task: "* Be able to show news, updates and menu",
+                responsable: "Lise Charlotte",
+                image: "images/liseChar.png"
+            },
+            {
+                name: "Design About page",
+                task: "* Info about, vision, company, offices, history",
+                responsable: "Nils Johan",
+                image: "images/nilsJohan.png"
+            },
+            {
+                name: "Design career page",
+                task: "* Post cards about diffrent positions",
+                responsable: "Reidun Lise",
+                image: "images/reidunLise.png"
+            }
+        ]
     }
 });
