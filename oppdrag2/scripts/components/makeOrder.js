60ef48d784902a48ca71d397f192275c90b6fc45
sannadianae <sanna-diana@hotmail.comm>
2019-04-02 15:32:18 +0200

    added calender

--- oppdrag2/scripts/components/makeOrder.js
+++ oppdrag2/scripts/components/makeOrder.js
@@ -6,15 +6,20 @@ Vue.component('make-order', {
             <input type="text" v-model="title">
 
             <label>Beskrivelse</label>
-            <input type="text">
+            <input type="text" v-model="desc">          
+
+            <label>Lokasjon</label>
+            <select type="text" v-model="location">
+                <option value="Afganistan">Afganistan</option>
+                <option value="USA">Amerikas forente stater</option>
+                <option value="Russland">Russland</option>
+                <option value="Syria">Syria</option>
+            </select>
 
             <label>Dato</label>
-            <input type="text">
-
-            <div id="lokasjon">Lokasjon
-                <input type="text">
+            <input type="text" autocomplete="off" id="datepicker" v-model="date">
             
-            </div>
+        
         </form>
         <input type="button" value="Slett">
         <input type="button" value="Send ordre" v-on:click="submitOrder">
@@ -23,13 +28,20 @@ Vue.component('make-order', {
     data() {
         return {
             title: "",
+            desc: "",
+            date: "",
+            location: ""
 
         }
     },
     methods: {
         submitOrder() {
             const orderData = {
-                title: this.title
+                title: this.title,
+                desc: this.desc,
+                date: this.date,
+                location: this.location
+
             }
             this.$emit('new-order', orderData)
         }
--- oppdrag2/scripts/components/order.js
+++ oppdrag2/scripts/components/order.js
@@ -2,11 +2,17 @@ Vue.component('order', {
     name: 'Order',
     template: `<div class="order-box">
         <h1>{{ title }}</h1>
+        <p>{{ desc }} </p>
+        <p>{{ date }} </p>
+        <p>{{ location }}</p>
         
         
     </div>
     `,
     props: {
-        title: String
+        title: String,
+        desc: String,
+        date: String,
+        location: String
     }
 })
