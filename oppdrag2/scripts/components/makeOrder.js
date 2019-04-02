9fd9fec7ba747576b78c7c0e08c1854fd6cf255a
sannadianae <sanna-diana@hotmail.comm>
2019-04-02 18:43:48 +0200

    steg videre

--- oppdrag2/scripts/components/makeOrder.js
+++ oppdrag2/scripts/components/makeOrder.js
@@ -8,16 +8,17 @@ Vue.component('make-order', {
             <label>Beskrivelse</label>
             <input type="text" v-model="desc">          
 
+            <label>Dato</label>
+            <input type="text" autocomplete="off" id="datepicker" v-model="date">
+
             <label>Lokasjon</label>
             <select type="text" v-model="location">
                 <option value="Afganistan">Afganistan</option>
-                <option value="USA">Amerikas forente stater</option>
+                <option value="Amerikas forente stater">Amerikas forente stater</option>
                 <option value="Russland">Russland</option>
                 <option value="Syria">Syria</option>
             </select>
-
-            <label>Dato</label>
-            <input type="text" autocomplete="off" id="datepicker" v-model="date">
+            
             
         
         </form>
@@ -31,7 +32,6 @@ Vue.component('make-order', {
             desc: "",
             date: "",
             location: ""
-
         }
     },
     methods: {
@@ -41,9 +41,22 @@ Vue.component('make-order', {
                 desc: this.desc,
                 date: this.date,
                 location: this.location
-
             }
             this.$emit('new-order', orderData)
+            this.title = ""
+            this.desc = ""
+            this.date = ""
+            this.location = ""
         }
+    },
+    mounted() {
+        $("#datepicker").datepicker({
+            onSelect: (dateText) => {
+                this.date = dateText
+            }
+        });
+    },
+    beforeDestory() {
+        $("#datepicker").datepicker('destroy');
     }
 })
--- oppdrag2/scripts/components/order.js
+++ oppdrag2/scripts/components/order.js
@@ -4,15 +4,24 @@ Vue.component('order', {
         <h1>{{ title }}</h1>
         <p>{{ desc }} </p>
         <p>{{ date }} </p>
-        <p>{{ location }}</p>
-        
-        
+        <p>{{ location }} </p>
+        <button><i class="fas fa-map-marker-alt"></i></button>
+        <img src="./images/afganistan.png"/>
     </div>
     `,
     props: {
         title: String,
         desc: String,
         date: String,
-        location: String
+        location: String,
+    },
+    methods: {
+        renderPicture(location) {
+            switch (location) {
+                case "Afganistan":
+                    return "./images/afganistan.png"
+            }
+            console.log("Sanna is awesome")
+        }
     }
 })
