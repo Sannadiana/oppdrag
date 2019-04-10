d705eb8b96d9f3df63d960b6bc8eb9171e30a0f6
sannadianae <sanna-diana@hotmail.comm>
2019-04-10 20:41:17 +0200

    oppdatering

---
+++ oppdrag3/scripts/shopping-cart.js
@@ -0,0 +1,36 @@
+Vue.component('shopping-cart', {
+    name: 'shopping-cart',
+    props: {
+        cart: Array,
+        products: Array,
+    },
+    computed: {
+        productsInCart() {
+            return this.products
+                .map(product => {
+                    return {
+                        ...product,
+                        variants: product.variants.map(variant => {
+                            return {
+                                ...variant,
+                                count: this.cart.filter(variantIdString => variantIdString === `${product.productId}-${variant.variantId}`).length
+                            }
+                        }).filter(variant => variant.count > 0)
+                    }
+                })
+                .filter(product => product.variants.length > 0)
+        }
+    },
+    template: `<div>
+        <ul>
+            <li v-for="product in productsInCart">
+                {{product.product}}
+                <ul>
+                    <li v-for="variant in product.variants">
+                        {{variant.variantColor}} - {{variant.count}}
+                    </li>
+                </ul>
+            </li> 
+        </ul>
+    </div>`
+})
