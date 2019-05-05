3b15d1aab1f35df170f73058bbf2f9aa1eff4b0e
sannadianae <sanna-diana@hotmail.comm>
2019-05-05 19:18:17 +0200

    oppdater

--- oppdrag2/scripts/components/solider-detail.js
+++ oppdrag2/scripts/components/solider-detail.js
@@ -3,10 +3,26 @@ Vue.component('solider-detail', {
     <div class="solider">
     <div class="solider-info">
         <h1>{{ name }}</h1>
+        
     </div>
     <div class="solider-image">
-        <img v-bind:src="image" alt="">
+        <img v-bind:src="soliderImage" alt="">
     </div>
+    <h1>{{ soliderLocation }}</h1>
 </div>
-    `
+    `,
+    props: {
+        soliderId: {
+            type: Number,
+        },
+        name: {
+            type: String,
+        },
+        soliderImage: {
+            type: String,
+        },
+        soliderLocation: {
+            type: String,
+        }
+    }
 })
--- oppdrag2/scripts/components/solider.js
+++ oppdrag2/scripts/components/solider.js
@@ -4,12 +4,25 @@ let miliApp = new Vue({
         soliders: [{
                 soliderId: 1,
                 name: "Mikkel",
-                soliderImage: "./images/soldier/mikkel.jpg"
+                soliderImage: "./images/soldier/mikkel.jpg",
+                soliderLocation: "Afganistan"
             },
             {
                 soliderId: 2,
                 name: "Nils",
-                soliderImage: "./images/soldier/nils.jpg"
+                soliderImage: "./images/soldier/nils.jpg",
+                soliderLocation: "Russland"
+            }, {
+                soliderId: 3,
+                name: "Felix",
+                soliderImage: "./images/soldier/felix.jpg",
+                soliderLocation: "Amerikas forente stater"
+            },
+            {
+                soliderId: 4,
+                name: "Per",
+                soliderImage: "./images/soldier/per.jpg",
+                soliderLocation: "Syria"
             }
         ]
     }
